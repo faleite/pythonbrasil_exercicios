2022872b586d67b2bedff0331bb54b67eb6a2689
faleite <fabricio_2310@hotmail.com>
2021-02-18 20:34:44 +0000

    Instalado flake8 para conferir qualidade de código

--- 1_estrutura_sequencial/13.py
+++ 1_estrutura_sequencial/13.py
@@ -4,11 +4,12 @@ utilizando as seguintes fórmulas:
 a. Para homens: (72.7*h) - 58
 b. Para mulheres: (62.1*h) - 44.7 """
 
-altura = float(input('Insira sua altura: '))
-homem = (72.7 * h) - 58
-mulher = (62.1 * h) - 44.7
+sexo = str(input('Insira seu sexo (M/F): '))
+h = float(input('Insira sua altura: '))
 
-for altura in
+if sexo == 'M':
+    peso = (72.7 * h) - 58
+else:
+    peso = (62.1 * h) - 44.7
 
-print(f'O peso ideal para homem é: {homem:.3f}kg')
-print(f'O peso ideal para mulher é: {mulher:.3f}kg')
+print(f'Seu peso ideal é: {peso:.3f}kg')
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Lista de exercícios Python Brasil
 Repositório para resolução de exercícios disponíveis na comunidade [Python Brasil](https://wiki.python.org.br/ListaDeExercicios)
 
-Suportada versão Python 3.9.1 
+Suportada versão Python 3.9.1
+
+Para conferir qualidade de código:
+
+```flake8```
