027c9dccbb48384412d9881dde1ccd1ef0c4a12c
Fabrício Araujo <fabricio_2310@hotmail.com>
2021-04-14 00:29:58 +0100

    Feito exercicios 17 em Estrutura de Decisao

--- 3_estrutura_de_repeticao/17.py
+++ 3_estrutura_de_repeticao/17.py
@@ -2,3 +2,21 @@
 17. Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
 Ex.: 5!=5.4.3.2.1=120
 """
+
+num = int(input('Insira o número: '))
+
+resultado = 1
+count = 1
+
+for n in range(1, num + 1):
+ 	resultado *= n
+print(f'{num}! = {resultado}')
+
+""" Com while """
+# resultado = 1
+# count = 1
+
+# while count <= num:
+#	resultado *= count
+#	count += 1
+# print(resultado)
