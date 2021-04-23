d268f480ad4ff377be83bf1b83c635fa137a47b7
faleite <fabricio_2310@hotmail.com>
2021-04-23 12:25:25 +0100

    Feito exercicios 27 e 28 em Estrutura de Repetição. Inserido enunciados 29 a 33.

--- 3_estrutura_de_repeticao/27.py
+++ 3_estrutura_de_repeticao/27.py
@@ -3,3 +3,21 @@
 Para isto, peça a quantidade de turmas e a quantidade de alunos para cada turma.
 As turmas não podem ter mais de 40 alunos.
 """
+
+turma = int(input('Insira a quantidade de turmas: '))
+
+som_alun = 0
+media = 0
+
+for i in range(1, turma + 1):
+    while True:
+        alunos = int(input(f'Insira a quantidade de alunos da {i}ª turma: '))
+        if alunos <= 40:
+            break
+        else:
+            print('As turmas não podem ter mais de 40 alunos.')
+            print('Tente novamente.')
+    som_alun += alunos
+    media = som_alun / turma
+
+print(f'O número médio de alunos por turma é: {media:.2f}')
--- 3_estrutura_de_repeticao/28.py
+++ 3_estrutura_de_repeticao/28.py
@@ -3,3 +3,15 @@
 e o valor médio gasto em cada um deles. O usuário deverá informar a quantidade
 de CDs e o valor para em cada um.
 """
+cd = int(input('Informe a quantidade de CDs: '))
+
+v_total = 0
+media = 0
+
+for count in range(1, cd + 1):
+    valor = float(input(f'Informe o valor do {count}º CD: '))
+    v_total += valor
+    media = v_total / cd
+
+print(f'Valor total investido: $ {v_total:.2f}')
+print(f'Valor médio gasto em cada CD: $ {media:.2f}')
