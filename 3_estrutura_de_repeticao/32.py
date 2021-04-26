494cf64953aad8b1cb58a15c21d14382cb583f27
faleite <fabricio_2310@hotmail.com>
2021-04-26 12:23:21 +0100

    Feito exercicios 32 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/32.py
+++ 3_estrutura_de_repeticao/32.py
@@ -4,3 +4,28 @@ Ex.: 5!=5.4.3.2.1=120. A saída deve ser conforme o exemplo abaixo:
 Fatorial de: 5
 5!= 5.4.3.2.1=120
 """
+
+
+def fatorial(num):
+    print('{}!='.format(num), end=' ')
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+        print(c, end='')
+        print('.' if c > 1 else '=', end='')
+    return f
+
+
+n = int(input('Fatorial de: '))
+print(fatorial(n))
+
+"""Com while"""
+# n = int(input('Fatorial de: '))
+# f = 1
+# print('{}!='.format(n), end=' ')
+# while n > 0:
+#     print(n, end='')
+#     print('.' if n > 1 else '=', end='')
+#     f *= n
+#     n -= 1
+# print(f)
