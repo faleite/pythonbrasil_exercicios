9b8d4e6cfced3125814bb1022d63f938378f9da8
faleite <fabricio_2310@hotmail.com>
2021-04-28 11:45:45 +0100

    Feito exercicios 33 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/33.py
+++ 3_estrutura_de_repeticao/33.py
@@ -3,3 +3,28 @@
 desenvolver um programa que leia as um conjunto indeterminado de temperaturas,
 e informe ao final a menor e a maior temperaturas informadas, bem como a média das temperaturas.
 """
+
+maior = 0
+menor = 0
+i = 1
+soma = 0
+print('Digite 00 para sair.')
+
+while True:
+    temp = float(input('Temperatura: '))
+    if temp == 00:
+        break
+    if i == 1:
+        menor = temp
+    if temp > maior:
+        maior = temp
+    if temp <= menor:
+        menor = temp
+    i += 1
+    soma += temp
+    media = soma / (i - 1)
+
+print()
+print(f'Menor temperatura: {menor:.2f}')
+print(f'Maior temperatura: {maior:.2f}')
+print(f'Temperatura média: {media:.2f}')
