13efb343e0aa9a9cb4ca3b2c577a1f437c2589bb
faleite <fabricio_2310@hotmail.com>
2021-04-29 11:57:40 +0100

    Feito exercicios 34 e 35 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/34.py
+++ 3_estrutura_de_repeticao/34.py
@@ -4,3 +4,19 @@ por exemplo na Criptografia.
 Um número primo é aquele que é divisível apenas por um e por ele mesmo.
 Faça um programa que peça um número inteiro e determine se ele é ou não um número primo.
 """
+
+
+def num_primo(*args):
+    mult = 0
+    for i in range(num):
+        i += 1
+        if num % i == 0:
+            mult += 1
+    if mult <= 2:
+        return f'{num} é um número primo'
+    else:
+        return f'{num} não é um número primo'
+
+
+num = int(input('Insira um número inteiro: '))
+print(num_primo(num))
--- 3_estrutura_de_repeticao/35.py
+++ 3_estrutura_de_repeticao/35.py
@@ -3,3 +3,14 @@
 Faça um programa que gera uma lista dos números primos existentes
 entre 1 e um número inteiro informado pelo usuário.
 """
+num = int(input('Insira um número: '))
+
+for n in range(1, num + 1):
+    mult = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            mult += 1
+    if mult == 2:
+        print(f'{i} é um número primo')
+    else:
+        print(f'{i} não é um número primo')
