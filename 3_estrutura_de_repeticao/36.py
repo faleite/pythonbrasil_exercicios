5976f75201e78e1735bfd2720040a93f1ba83199
faleite <fabricio_2310@hotmail.com>
2021-05-05 12:04:43 +0100

    Feito exercicio 36 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/36.py
+++ 3_estrutura_de_repeticao/36.py
@@ -15,15 +15,18 @@ Vou montar a tabuada de 5 começando em 4 e terminando em 7:
 5 X 7 = 35
 Obs: Você deve verificar se o usuário não digitou o final menor que o inicial.
 """
+numero = int(input('Montar a tabuada de: '))
 
+while True:
+    comecar = int(input('Começar por: '))
+    terminar = int(input('Terminar em: '))
+    if comecar < terminar:
+        break
+    else:
+        print()
+        print('O número final, deve ser menor que o inicial. Tente novamente.')
 
-def tabuada():
-    for i in range(comeco, termino + 1):
-        mult = tabuada_de * i
-        print(f'{tabuada_de} X {i} = {mult}')
-
-
-tabuada_de = int(input('Montar a tabuada de: '))
-comeco = int(input('Começar por: '))
-termino = int(input('Terminar em: '))
-tabuada()
+print(f'Vou montar a tabuada de {numero} começando em {comecar} e terminando em {terminar}:')
+for i in range(comecar, terminar + 1):
+    mult = numero * i
+    print(f'{numero} X {i} = {mult}')
--- 3_estrutura_de_repeticao/37.py
+++ 3_estrutura_de_repeticao/37.py
@@ -3,6 +3,17 @@
 o mais baixo, a mais gordo e o mais magro, para isto você deve fazer um programa que
 pergunte a cada um dos clientes da academia seu código, sua altura e seu peso.
 O final da digitação de dados deve ser dada quando o usuário digitar 0 (zero) no campo código.
-Ao encerrar o programa também deve ser informados os códigos e valores do clente mais alto,
+Ao encerrar o programa também deve ser informados os códigos e valores do cliente mais alto,
 do mais baixo, do mais gordo e do mais magro, além da média das alturas e dos pesos dos clientes
 """
+
+while True:
+    codigo = int(input('Insira seu código: '))
+    altura = float(input('Insira sua altura: '))
+    peso = float(input('Insira seu peso: '))
+    if codigo > 0:
+        break
+    else:
+        while altura > altura:
+            print('Cliente mais alto:')
+            print(f'Código: {codigo}, Altura: {altura} e Peso: {peso}')
