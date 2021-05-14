37d908a17548c451b15d31bd3360e98f0b44c102
faleite <fabricio_2310@hotmail.com>
2021-05-14 11:40:12 +0100

    Feito nova formula do exercicio 39 em Estrutura de Repetição. Feito parte do exercicio 40 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/39.py
+++ 3_estrutura_de_repeticao/39.py
@@ -8,23 +8,48 @@ junto com suas alturas.
 """
 
 mais_alto = mais_baixo = 0
-lista = []
+num_a = num_b = 0
 
 for i in range(1, 4):
     n_aluno = int(input('Informe o número do aluno: '))
     alt_aluno = int(input('Informe a altura do aluno em centímetros: '))
-    lista.append({'Aluno': n_aluno, 'Altura': alt_aluno})
 
-for y in lista:
-    if y['Altura'] > mais_alto:
-        a_alto = y['Aluno']
-        mais_alto = y['Altura']
-    if y['Altura'] < mais_alto:
-        a_baixo = y['Aluno']
-        mais_baixo = y['Altura']
+    if i == 1:
+        numero = n_aluno
+        altura = alt_aluno
+    if alt_aluno > altura:
+        num_a = n_aluno
+        mais_alto = alt_aluno
+    if alt_aluno < altura:
+        num_b = n_aluno
+        mais_baixo = alt_aluno
+
 
 print('Aluno mais alto:')
-print(f'Aluno: {a_alto}, Altura: {mais_alto} cm')
+print(f'Aluno: {num_a}, Altura: {mais_alto} cm')
 
 print('Aluno mais baixo:')
-print(f'Aluno: {a_baixo}, Altura: {mais_baixo} cm')
+print(f'Aluno: {num_b}, Altura: {mais_baixo} cm')
+
+""" Outra forma """
+# mais_alto = mais_baixo = 0
+# lista = []
+#
+# for i in range(1, 11):
+#     n_aluno = int(input('Informe o número do aluno: '))
+#     alt_aluno = int(input('Informe a altura do aluno em centímetros: '))
+#     lista.append({'Aluno': n_aluno, 'Altura': alt_aluno})
+#
+# for y in lista:
+#     if y['Altura'] > mais_alto:
+#         a_alto = y['Aluno']
+#         mais_alto = y['Altura']
+#     if y['Altura'] < mais_alto:
+#         a_baixo = y['Aluno']
+#         mais_baixo = y['Altura']
+#
+# print('Aluno mais alto:')
+# print(f'Aluno: {a_alto}, Altura: {mais_alto} cm')
+#
+# print('Aluno mais baixo:')
+# print(f'Aluno: {a_baixo}, Altura: {mais_baixo} cm')
--- 3_estrutura_de_repeticao/40.py
+++ 3_estrutura_de_repeticao/40.py
@@ -8,3 +8,28 @@ d. Qual o maior e menor índice de acidentes de transito e a que cidade pertence
 e. Qual a média de veículos nas cinco cidades juntas;
 f. Qual a média de acidentes de trânsito nas cidades com menos de 2.000 veículos de passeio.
 """
+
+c_menor_acid = menor_acid = 0
+c_maior_acid = maior_acid = 0
+
+for i in range(1, 4):
+    cid = int(input('Informe o código da cidade: '))
+    veic = int(input('Informe o número de veículos de passeio: '))
+    acid = int(input('Informe o número de acidentes de trânsito com vítimas: '))
+
+    if i == 1:
+        c_menor_acid = cid
+        menor_acid = acid
+    if acid > maior_acid:
+        c_maior_acid = cid
+        maior_acid = acid
+    if acid < menor_acid:
+        c_menor_acid = cid
+        menor_acid = acid
+
+print()
+print(f'O maior índice de acidentes de transito é: {maior_acid}, e pertence a cidade: {c_maior_acid} ')
+print(f'O menor índice de acidentes de transito é: {menor_acid}, e pertence a cidade: {c_menor_acid} ')
+print()
+print(f'Média de veículos nas cinco cidades juntas: ')
+print(f'Média de acidentes de trânsito nas cidades com menos de 2.000: ')
