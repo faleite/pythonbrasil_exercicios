ebed4705735914bf38273f60b8a6940569f7db06
faleite <fabricio_2310@hotmail.com>
2021-05-25 12:14:53 +0100

    atualizado exercicio 43 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/43.py
+++ 3_estrutura_de_repeticao/43.py
@@ -8,6 +8,26 @@ Hambúrguer      103     R$ 1,20
 Cheeseburguer   104     R$ 1,30
 Refrigerante    105     R$ 1,00
 
-Faça um programa que leia o código dos itens pedidos e as quantidades desejadas. Calcule e mostre o valor a ser pago por item (preço * quantidade) e o total geral do
-pedido. Considere que o cliente deve informar quando o pedido deve ser encerrado.
+Faça um programa que leia o código dos itens pedidos e as quantidades desejadas.
+Calcule e mostre o valor a ser pago por item (preço * quantidade) e o total geral do pedido.
+Considere que o cliente deve informar quando o pedido deve ser encerrado.
 """
+
+while True:
+    codigo = int(input('Informe o código do pedido: '))
+    if codigo == 0:
+        break
+    qtd = int(input('Informe a quantidade: '))
+
+    if codigo == 100:
+        print(f'Valor a ser pago: {qtd*1.20}')
+    if codigo == 101:
+        print(f'Valor a ser pago: {qtd*1.30}')
+    if codigo == 102:
+        print(f'Valor a ser pago: {qtd*1.50}')
+    if codigo == 103:
+        print(f'Valor a ser pago: {qtd*1.20}')
+    if codigo == 104:
+        print(f'Valor a ser pago: {qtd*1.30}')
+    if codigo == 105:
+        print(f'Valor a ser pago: {qtd*1.00}')
