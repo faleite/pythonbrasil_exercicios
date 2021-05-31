3ed0dcc85092190ab3de268bc42e071851c72cbe
faleite <fabricio_2310@hotmail.com>
2021-05-31 11:31:22 +0100

    Feito exercicio 46 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/46.py
+++ 3_estrutura_de_repeticao/46.py
@@ -22,3 +22,47 @@ Média dos demais saltos: 5.9 m
 Resultado final:
 Rodrigo Curvêllo: 5.9 m
 """
+
+while True:
+    saltos = []
+    atleta = str(input('Atleta: '))
+    if atleta == '':
+        break
+    for s in range(1, 6):
+        salto = float(input(f'Informe o {s}º Salto: '))
+        saltos.append(salto)
+    print()
+    print(f'Atleta: {atleta}')
+    print(f'Primeiro Salto: {saltos[0]} m')
+    print(f'Segundo Salto: {saltos[1]} m')
+    print(f'Terceiro Salto: {saltos[2]} m')
+    print(f'Quarto Salto: {saltos[3]} m')
+    print(f'Quinto Salto: {saltos[4]} m')
+
+    saltos.sort()
+    maior = saltos[-1]
+    menor = saltos[0]
+    saltos = saltos[1:4]  # O mesmo que saltos[1:-1]
+
+    media = sum(saltos) / 3
+
+    # Outra forma para extrair maior, menor e média
+
+    # maior = menor = lst[0]
+    #
+    # for valor in lst:
+    #     if valor > maior:
+    #         maior = valor
+    #     if valor < menor:
+    #         menor = valor
+    # lst.remove(maior)
+    # lst.remove(menor)
+
+    # media = sum(lst) / 3
+
+    print(f'Melhor salto: {maior} m')
+    print(f'Pior salto: {menor} m')
+    print(f'Média dos demais saltos: {media:.2f} m')
+    print('Resultado final:')
+    print(f'{atleta}: {media:.2f} m')
+    print()
