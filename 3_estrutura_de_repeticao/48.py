a67b7adad2f9f1ae475867ef9e0b649bd7c59919
faleite <fabricio_2310@hotmail.com>
2021-06-09 22:58:44 +0100

    Feito exercicio 48 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/48.py
+++ 3_estrutura_de_repeticao/48.py
@@ -6,3 +6,13 @@ Exemplo:
 12376489
 => 98467321
 """
+
+n = int(input('Informe um número inteiro positivo: '))
+
+# Forma com função .join e reversed
+num_str = str(n)
+print(''.join(reversed(num_str)))
+
+# Forma com interação de lista na string
+num_str2 = str(n)[::-1]
+print(num_str2)
