b8c97e7d7f62c8ca876fb7244565363c9534d088
faleite <fabricio_2310@hotmail.com>
2021-07-11 17:24:02 +0100

    Feito exercicio 49 em Estrutura de Repetição.

--- 3_estrutura_de_repeticao/49.py
+++ 3_estrutura_de_repeticao/49.py
@@ -4,10 +4,9 @@
 S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m.
 Imprima no final a soma da série.
 """
-
 n = int(input('Informe o número de termos: '))
-
 num = razao = soma = 0
+
 for i in range(1, n + 1):
     if razao == 0:
         razao = 1
